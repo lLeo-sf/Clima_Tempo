a59133955d2cac38cd0f4ae7c5a4ee2fae15ac0e
Leonardo <leonardo_siqfernandes@outlook.com>
2021-05-23 00:05:14 -0300

    refatorando

--- app/src/server/index.js
+++ app/src/server/index.js
@@ -1,15 +1,14 @@
 const express = require('express')
-const app = express()
-const port = 3001
 const cors = require('cors')
 const routes = require('./routes').router;
+
+const app = express()
+const port = 3001
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use('/', routes)
-
-
 
 
 app.listen(port, () => { })
